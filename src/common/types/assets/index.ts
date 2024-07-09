0c46c85446080ec5b85f3e8080ce5eab4f82c7ff
Nikolai Kapitonov <Koluankapitonov@mail.ru>
2024-07-09 09:22:22 +0300

    Added types chart components

--- src/common/types/assets/index.ts
+++ src/common/types/assets/index.ts
@@ -12,41 +12,133 @@ interface Roi {
   percentage: number;
 }
 
-export interface ISingleAsset {
-  id: string;
-  symbol: string;
-  name: string;
-  image: string;
-  current_price: number;
-  market_cap: any;
-  market_cap_rank: number;
-  fully_diluted_valuation: any;
-  total_volume: any;
-  high_24h: number;
-  low_24h: number;
-  price_change_24h: number;
-  price_change_percentage_24h: number;
-  market_cap_change_24h: number;
-  market_cap_change_percentage_24h: number;
+interface CommunityData {
+  facebook_likes: number | null;
+  twitter_followers: number;
+  reddit_average_posts_48h: number;
+  reddit_average_comments_48h: number;
+  reddit_subscribers: number;
+}
+
+interface DeveloperData {
+  forks: number;
+  stars: number;
+  subscribers: number;
+  total_issues: number;
+  closed_issues: number;
+  // Добавьте другие свойства по мере необходимости
+}
+
+interface Image {
+  thumb: string;
+  small: string;
+  large: string;
+}
+
+interface Links {
+  homepage: string[];
+  whitepaper: string;
+  blockchain_site: string[];
+  official_forum_url: string[];
+  chat_url: string[];
+  // Добавьте другие свойства по мере необходимости
+}
+
+interface Localization {
+  en: string;
+  de: string;
+  es: string;
+  fr: string;
+  it: string;
+  // Добавьте другие языки по мере необходимости
+}
+
+interface CurrencyData {
+  [key: string]: number;
+}
+
+interface CurrencyDateData {
+  [key: string]: string;
+}
+
+interface MarketData {
+  ath: CurrencyData;
+  ath_change_percentage: CurrencyData;
+  ath_date: CurrencyDateData;
+  atl: CurrencyData;
+  atl_change_percentage: CurrencyData;
+  atl_date: CurrencyDateData;
   circulating_supply: number;
+  current_price: CurrencyData;
+  fdv_to_tvl_ratio: number | null;
+  fully_diluted_valuation: CurrencyData;
+  high_24h: CurrencyData;
+  last_updated: string;
+  low_24h: CurrencyData;
+  market_cap: CurrencyData;
+  market_cap_change_24h: number;
+  market_cap_change_24h_in_currency: CurrencyData;
+  market_cap_change_percentage_24h: number;
+  market_cap_change_percentage_24h_in_currency: CurrencyData;
+  market_cap_fdv_ratio: number;
+  market_cap_rank: number;
+  max_supply: number;
+  mcap_to_tvl_ratio: number | null;
+  price_change_24h: number;
+  price_change_24h_in_currency: CurrencyData;
+  price_change_percentage_1h_in_currency: CurrencyData;
+  price_change_percentage_1y: number;
+  price_change_percentage_1y_in_currency: CurrencyData;
+  price_change_percentage_7d: number;
+  price_change_percentage_7d_in_currency: CurrencyData;
+  price_change_percentage_14d: number;
+  price_change_percentage_14d_in_currency: CurrencyData;
+  price_change_percentage_24h: number;
+  price_change_percentage_24h_in_currency: CurrencyData;
+  price_change_percentage_30d: number;
+  price_change_percentage_30d_in_currency: CurrencyData;
+  price_change_percentage_60d: number;
+  price_change_percentage_60d_in_currency: CurrencyData;
+  price_change_percentage_200d: number;
+  price_change_percentage_200d_in_currency: CurrencyData;
+  roi: number | null;
   total_supply: number;
-  max_supply?: number;
-  ath: number;
-  ath_change_percentage: number;
-  ath_date: Date;
-  atl: number;
-  atl_change_percentage: number;
-  atl_date: Date;
-  roi: Roi;
-  last_updated: Date;
+  total_value_locked: number | null;
+  total_volume: CurrencyData;
 }
-
-export interface IChartData {
+interface Data {
+  additional_notices: any[];
+  asset_platform_id: string | null;
+  block_time_in_minutes: number;
+  categories: string[];
+  community_data: CommunityData;
+  country_origin: string;
+  description: Record<string, string>;
+  detail_platforms: Record<string, any>;
+  developer_data: DeveloperData;
+  genesis_date: string;
+  hashing_algorithm: string;
+  id: string;
+  image: Image;
+  last_updated: string;
+  links: Links;
+  localization: Localization;
+  market_cap_rank: number;
+  market_data: MarketData;
   name: string;
-  price_chart_data: number[][];
-  singleAsset: ISingleAsset[];
+  platforms: Record<string, string>;
+  preview_listing: boolean;
+  public_notice: string | null;
+  sentiment_votes_down_percentage: number;
+  sentiment_votes_up_percentage: number;
+  status_updates: any[];
+  symbol: string;
+  tickers: any[];
+  watchlist_portfolio_users: number;
+  web_slug: string;
 }
 
-export interface ITablePriceData {
-  assets: ISingleAsset[];
+export interface Cryptocurrency {
+  name: string;
+  data: Data;
 }
--- src/pages/home/index.tsx
+++ src/pages/home/index.tsx
@@ -7,8 +7,9 @@ import AreaChart from "../../components/charts/area-chart";
 import TrendUp from "../../assets/images/chart/trend-up.svg";
 import TrendDown from "../../assets/images/chart/trend-down.svg";
 import LineChart from "../../components/charts/line-chart";
+import { Cryptocurrency } from "../../common/types/assets";
 
-export default function Home() {
+export default function Home(): JSX.Element {
   const dispatch = useAppDispatch();
   const favoriteAssets: [] = useAppSelector(
     (store) => store.assets.favoriteAssets
@@ -38,9 +39,6 @@ export default function Home() {
     [dispatch]
   );
 
-  // useEffect(() => {
-  //   fetchDataPrices(favoriteAssetsId);
-  // }, []);
   useEffect(() => {
     if (fetchDataRef.current) return;
     fetchDataRef.current = true;
@@ -48,49 +46,48 @@ export default function Home() {
     fetchDataPrices(favoriteAssetsId);
   }, [favoriteAssetsId, fetchData]);
 
-  const renderFavoriteBlock = filteredArray.map((element: any, i) => {
-    // console.log(element.data.market_data);
-
-    const currentPrice = element.data.market_data.current_price.usd;
-    const currentCup = element.data.market_data.market_cap.usd;
-    const changePrice =
-      element.data.market_data.market_cap_change_percentage_24h;
-    return (
-      <Grid item xs={12} sm={6} lg={6} key={element.name}>
-        <Grid container className={classes.topCardItem}>
-          <Grid item xs={12} sm={6} lg={6} sx={{ height: "185px" }}>
-            <h3 className={classes.assetName}>{element.name}</h3>
-            <div className={classes.itemDetails}>
-              <span className={classes.cardPrice}>
-                ${currentPrice.toFixed(2)}
-              </span>
-              <Box
-                className={
-                  changePrice > 0
-                    ? `${classes.priceTrend} ${classes.trendUp}`
-                    : `${classes.priceTrend} ${classes.trendDown}`
-                }
-              >
-                {changePrice > 0 ? (
-                  <img src={TrendUp} alt="TrendUp" />
-                ) : (
-                  <img src={TrendDown} alt="TrendDown" />
-                )}
-                <Typography variant="body1">
-                  {Number(changePrice).toFixed(2)}%
-                </Typography>
-              </Box>
-            </div>
-          </Grid>
-          <Grid item xs={12} sm={6} lg={6} className={classes.areaGrid}>
-            {pricesAssets.length && pricesAssets[i]?.prices && (
-              <AreaChart data={pricesAssets[i]?.prices} />
-            )}
+  const renderFavoriteBlock = filteredArray.map(
+    (element: Cryptocurrency, i: number) => {
+      const currentPrice = element.data.market_data.current_price.usd;
+      const changePrice =
+        element.data.market_data.market_cap_change_percentage_24h;
+      return (
+        <Grid item xs={12} sm={6} lg={6} key={element.name}>
+          <Grid container className={classes.topCardItem}>
+            <Grid item xs={12} sm={6} lg={6} sx={{ height: "185px" }}>
+              <h3 className={classes.assetName}>{element.name}</h3>
+              <div className={classes.itemDetails}>
+                <span className={classes.cardPrice}>
+                  ${currentPrice.toFixed(2)}
+                </span>
+                <Box
+                  className={
+                    changePrice > 0
+                      ? `${classes.priceTrend} ${classes.trendUp}`
+                      : `${classes.priceTrend} ${classes.trendDown}`
+                  }
+                >
+                  {changePrice > 0 ? (
+                    <img src={TrendUp} alt="TrendUp" />
+                  ) : (
+                    <img src={TrendDown} alt="TrendDown" />
+                  )}
+                  <Typography variant="body1">
+                    {Number(changePrice).toFixed(2)}%
+                  </Typography>
+                </Box>
+              </div>
+            </Grid>
+            <Grid item xs={12} sm={6} lg={6} className={classes.areaGrid}>
+              {pricesAssets.length && pricesAssets[i]?.prices && (
+                <AreaChart data={pricesAssets[i]?.prices} />
+              )}
+            </Grid>
           </Grid>
         </Grid>
-      </Grid>
-    );
-  });
+      );
+    }
+  );
   return (
     <Box className={classes.root}>
       <Grid container spacing={2} className={classes.areaChart}>
