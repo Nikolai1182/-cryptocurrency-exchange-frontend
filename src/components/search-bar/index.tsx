37dc23539dd1469147a8485ff22015d98a469b66
Nikolai Kapitonov <Koluankapitonov@mail.ru>
2024-07-09 20:23:18 +0300

    create single page

--- src/components/search-bar/index.tsx
+++ src/components/search-bar/index.tsx
@@ -2,15 +2,22 @@ import React, { FC, useState } from "react";
 import { Stack, Autocomplete, TextField } from "@mui/material";
 import SearchIcon from "@mui/icons-material/Search";
 import { useAppSelector } from "../../utils/hook";
+import { useNavigate } from "react-router-dom";
 
 const SearchBarComponent: FC = (): JSX.Element => {
+  const navigate = useNavigate();
+  const [selectedItem, setSelectedItem] = useState<string | null>("");
   const topPrice = useAppSelector((store) => store.assets.topPricesCoins);
-  console.log(topPrice);
+  console.log(selectedItem);
 
   return (
     <Stack spacing={2} sx={{ width: "300px" }}>
       <Autocomplete
-        freeSolo
+        value={selectedItem}
+        onChange={(e: any, value: string | null) => {
+          navigate(`single/${value}`);
+          setSelectedItem("");
+        }}
         renderInput={(element) => (
           <TextField
             {...element}
